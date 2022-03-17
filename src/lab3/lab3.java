2916663ba83e9b0f5ae97d67d6af582c456089fd
vladzubcu228 <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>
2022-03-17 12:02:20 +0200

    initial commit

---
+++ src/lab3/lab3.java
@@ -0,0 +1,10 @@
+package lab3;
+
+public class lab3 {
+
+	public static void main(String[] args){
+		System.out.println("Hello World");
+		System.out.println("How are you?");
+		System.out.println("1");
+	}
+}
