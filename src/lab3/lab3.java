577be8d2f5b80926c97336b7ef59b43de6f160f3
adamf <adamf@LAPTOP-9036782A>
2022-05-06 00:00:57 +0300

    commit 90

--- src/lab3/lab3.java
+++ src/lab3/lab3.java
@@ -6,6 +6,8 @@ public class lab3 {
 		System.out.println("Hello World");
 		System.out.println("How are you?");
 		System.out.println("How are you?");
+		System.out.println("=========");
+		System.out.println("Fine!");
 		
 	}
 }
