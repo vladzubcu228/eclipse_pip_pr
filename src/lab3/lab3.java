9b0ad715e7c84a3fb2772b622f01e9acc70e527c
vladzubcu228 <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>
2022-05-05 20:43:10 +0300

    commit 6

--- src/lab3/lab3.java
+++ src/lab3/lab3.java
@@ -10,6 +10,9 @@ public class lab3 {
 		System.out.println("3");
 		System.out.println("3");
 		System.out.println("3");
-		
+		System.out.println("2");
+		System.out.println("3");
+		System.out.println("3");
+		System.out.println("3");
 	}
 }
