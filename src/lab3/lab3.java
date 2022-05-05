ea186c88333603a8492e7dbe976de4f0da664bfc
vladzubcu228 <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>
2022-05-05 20:46:27 +0300

    commit 8

--- src/lab3/lab3.java
+++ src/lab3/lab3.java
@@ -5,19 +5,6 @@ public class lab3 {
 	public static void main(String[] args){
 		System.out.println("Hello World");
 		System.out.println("How are you?");
-		System.out.println("1");
-		System.out.println("2");
-		System.out.println("3");
-		System.out.println("3");
-		System.out.println("3");
-		System.out.println("2");
-		System.out.println("3");
-		System.out.println("3");
-		System.out.println("3");
-		System.out.println("2");
-		System.out.println("5");
-		System.out.println("5");
-		System.out.println("5");
 		
 	}
 }
